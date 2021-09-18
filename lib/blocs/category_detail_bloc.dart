0f099791f795fb7a821c8b8b10d87aa4faa424ff
Thien Vu <clarktn297@gmail.com>
2021-09-18 11:10:30 +0700

    [Mobile][UID-5-6-7][SCSS-57-58-59] add BLoC
    category detail and edit category detail

---
+++ lib/blocs/category_detail_bloc.dart
@@ -0,0 +1,33 @@
+import 'package:dealer_app/repositories/events/category_detail_event.dart';
+import 'package:dealer_app/repositories/states/category_detail_state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:image_picker/image_picker.dart';
+
+class CategoryDetailBloc
+    extends Bloc<CategoryDetailEvent, CategoryDetailState> {
+  final _picker = ImagePicker();
+
+  CategoryDetailBloc(CategoryDetailState initialState) : super(initialState);
+
+  @override
+  Stream<CategoryDetailState> mapEventToState(
+      CategoryDetailEvent event) async* {
+    if (event is EventChangeScrapImageRequest) {
+      yield state.copyWith(isImageSourceActionSheetVisible: true);
+    }
+    if (event is EventOpenImagePicker) {
+      yield state.copyWith(isImageSourceActionSheetVisible: false);
+      final pickedImage = await _picker.pickImage(source: event.imageSource);
+      if (pickedImage != null) {
+        yield state.copyWith(pickedImageUrl: pickedImage.path);
+      } else
+        return;
+    }
+    if (event is EventAddScrapCategoryUnit) {
+      yield state.copyWith(controllers: event.controllers);
+    }
+    if (event is EventChangeEditStatus) {
+      yield state.copyWith(isEdited: event.isEdited);
+    }
+  }
+}
