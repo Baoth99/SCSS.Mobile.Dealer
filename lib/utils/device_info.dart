83804c249f99d8393dcc79b8009e00f1e5709723
Thien Vu <clarktn297@gmail.com>
2021-11-08 20:13:16 +0700

    SCSS-152 change device id

--- lib/utils/device_info.dart
+++
@@ -1,9 +0,0 @@
-import 'package:device_info_plus/device_info_plus.dart';
-
-class DeviceInfo {
-  static Future<String?> getDeviceId() async {
-    DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
-    AndroidDeviceInfo androidInfo = await deviceInfo.androidInfo;
-    return androidInfo.androidId;
-  }
-}
