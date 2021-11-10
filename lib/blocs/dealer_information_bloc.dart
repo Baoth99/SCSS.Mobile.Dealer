a938ae9e26061c58121acbe20996eedcff8f3826
Tien <tienjtran@gmail.com>
2021-11-10 18:52:49 +0700

    [Mobile][UID-21][SCSS-171]

---
+++ lib/blocs/dealer_information_bloc.dart
@@ -0,0 +1,51 @@
+import 'package:dealer_app/log/logger.dart';
+import 'package:dealer_app/providers/configs/injection_config.dart';
+import 'package:dealer_app/providers/services/identity_server_service.dart';
+import 'package:dealer_app/repositories/events/dealer_information_event.dart';
+import 'package:dealer_app/repositories/states/dealer_information_state.dart';
+import 'package:dealer_app/utils/common_function.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:formz/formz.dart';
+
+class DealerInformationBloc
+    extends Bloc<DealerInformationEvent, DealerInformationState> {
+  DealerInformationBloc({IdentityServerService? identityServerService})
+      : super(DealerInformationState()) {
+    _identityServerService =
+        identityServerService ?? getIt.get<IdentityServerService>();
+  }
+
+  late IdentityServerService _identityServerService;
+
+  @override
+  Stream<DealerInformationState> mapEventToState(
+      DealerInformationEvent event) async* {
+    if (event is DealerInformationInitial) {
+      if (state.status != FormzStatus.submissionInProgress) {
+        try {
+          yield state.copyWith(
+            status: FormzStatus.submissionInProgress,
+          );
+
+          var newState = await futureAppDuration(
+              _identityServerService.getDealerInformation());
+
+          yield newState.copyWith(
+            status: FormzStatus.submissionSuccess,
+          );
+        } catch (e) {
+          AppLog.error(e);
+          yield state.copyWith(
+            status: FormzStatus.submissionFailure,
+          );
+        }
+      }
+    } else if (event is DealerInformationClear) {
+      try {
+        yield DealerInformationState();
+      } catch (e) {
+        AppLog.error(e);
+      }
+    }
+  }
+}
